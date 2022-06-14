ee90d91afe50e5aa8c9ef89d19759a225abc70b3
manuelblancovalentin <manuel.blanco.valentin@gmail.com>
2022-06-13 21:35:23 -0500

    Bug fixed: Support for single flow run. When passing a single flow step now wolf only runs that specific flow. It was running all steps after that one, before

--- bin/flow_yaml_parser.py
+++ bin/flow_yaml_parser.py
@@ -1,16 +1,26 @@
 import sys
 import yaml
 
-def recursive_key_search(substruct, cum=""):
+def recursive_key_search(substruct, cum="", current_flows=[]):
     subout = tuple()
     if isinstance(substruct, dict):
         for (k, v) in substruct.items():
             if k != "steps":
-                # Go straight to values
-                cumtmp = f"{cum}.{k}" if cum != "" else k
-                subout += (cumtmp,)
-                #print(f"dict: {cumtmp}")
-                subout += recursive_key_search(v, cum=cumtmp)
+                if cum == "":
+                    for j in current_flows:
+                        if k.startswith(j):
+                            # Go straight to values
+                            cumtmp = f"{k}"
+                            subout += (cumtmp,)
+                            #print(f"dict: {cumtmp}")
+                            subout += recursive_key_search(v, cum=cumtmp)
+                else:
+                    # Go straight to values
+                    cumtmp = f"{cum}.{k}"
+                    subout += (cumtmp,)
+                    #print(f"dict: {cumtmp}")
+                    subout += recursive_key_search(v, cum=cumtmp)
+                        
             else:
                 subout += recursive_key_search(v, cum=f"{cum}")
 
@@ -21,12 +31,13 @@ def recursive_key_search(substruct, cum=""):
 
 def main(args):
     filename = args[0]
+    current_flows = args[1:]
     with open(filename, "r") as stream:
         try:
             struct = yaml.safe_load(stream)
             # Now call recursive fun
             if 'flows' in struct:
-                array = recursive_key_search(struct['flows'])
+                array = recursive_key_search(struct['flows'], current_flows=current_flows)
                 for i in array:
                     print(i)
         except yaml.YAMLError as exc:
