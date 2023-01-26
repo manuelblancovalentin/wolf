6d62a8091b08a0be6524d600de5623495b7902a2
manuelblancovalentin <manuel.blanco.valentin@gmail.com>
2023-01-26 01:17:16 -0600

    Improved the template for floorplanning in tsmc65 and fixed minor bugs in the setup.yaml for tsmc65

--- templates/tsmc65/tsmc65.floorplan.template.tcl
+++ templates/tsmc65/tsmc65.floorplan.template.tcl
@@ -2,11 +2,34 @@
 # Get design name
 set design_name [get_flow_config design_name]
 
+# Temporarily report area to get the total area and calculate the width/height
+report_area > tmp.area
+set areas [exec tail -n 1 tmp.area]
+set total_area [lindex $areas 2]
+rm tmp.area
+
+# Set die_width and height to default minimum values according to total area
+set margin_horizontal 1.0
+set margin_vertical 1.0
+set die_width [expr sqrt($total_area/0.7) + 2*$margin_horizontal]
+set die_height [expr sqrt($total_area/0.7) + 2*$margin_vertical]
+
 # Create main floorplan with specific die size
-set die_width [get_flow_config floorplan_spef die_width]
-set die_height [get_flow_config floorplan_spef die_height]
-set margin_horizontal [get_flow_config floorplan_spef margin_horizontal]
-set margin_vertical [get_flow_config floorplan_spef margin_vertical]
+if { [get_flow_config -q floorplan_spef] ne "" } {
+
+    if { [get_flow_config -q floorplan_spef die_width] ne "" } {
+        set die_width [get_flow_config floorplan_spef die_width]
+    } 
+    if { [get_flow_config -q floorplan_spef die_height] ne "" } {
+        set die_height [get_flow_config floorplan_spef die_height]
+    } 
+    if { [get_flow_config -q floorplan_spef margin_horizontal] ne "" } {
+        set margin_horizontal [get_flow_config floorplan_spef margin_horizontal]
+    }
+    if { [get_flow_config -q floorplan_spef margin_vertical] ne "" } {
+        set margin_vertical [get_flow_config floorplan_spef margin_vertical]
+    }
+}
 
 # Delete pevious floorplan, in case
 set_io_flow_flag 0
@@ -26,7 +49,7 @@ create_floorplan \
     -flip s \
     -die_size $die_width $die_height $margin_horizontal $margin_vertical $margin_horizontal $margin_vertical \
     -core_margins_by io \
-    -site $core_site_name \
+    -site $core_site_name 
 
 puts "\033\[1;33m\[INFO\] - \033\[1;32mCreated \033\[1;31mfloorplan\033\[1;32m with size \033\[1;32m\{$die_width $die_height $margin_horizontal $margin_vertical $margin_horizontal $margin_vertical\}\033\[0m"
 
@@ -42,4 +65,110 @@ if { [expr [file readable $io_template_file] && [file exists $io_template_file] 
 
 }
 
-# Power grid, etc.
+######################
+# GLOBAL CONNECTS
+######################
+# MANU: GET all layers that we are going to use for the power grid
+set vlayers [get_db [get_db layers -if {.type == routing && .direction == vertical} ] .name]
+set hlayers [get_db [get_db layers -if {.type == routing && .direction == horizontal} ] .name]
+
+# Delete other previous routes (if any)
+set pg_layers {  } 
+
+
+if { [get_flow_config -q floorplan_spef power_grid layers vertical] ne "" } { 
+    set vlayers [get_flow_config floorplan_spef power_grid layers vertical]
+
+    for {set i 0} {$i < [ expr [llength $vlayers]/2]} {incr i} {
+        lappend pg_layers [lindex $vlayers [expr 2*$i] ]
+    }
+} else {
+    set pg_layers [list {*}$vlayers]
+}
+if { [get_flow_config -q floorplan_spef power_grid layers horizontal] ne "" } { 
+    set hlayers [get_flow_config floorplan_spef power_grid layers horizontal]
+    
+    for {set i 0} {$i < [ expr [llength $hlayers]/2]} {incr i} {
+        lappend pg_layers [lindex $hlayers [expr 2*$i] ]
+    }
+} else {
+    set pg_layers [list {*}$pg_layers {*}$hlayers]
+}
+
+set all_pg_nets [list {*}[get_flow_config init_power_nets] {*}[get_flow_config init_ground_nets]]
+select_routes -wires_only 0 -type special -nets $all_pg_nets -layer $pg_layers -shapes {stripe followpin}
+delete_routes -selected -type special -net $all_pg_nets -layer $pg_layers -shapes {stripe followpin}
+
+# The first step of power planning is to declare global connections: this consists in declaring the pins 
+# that connect to power and ground nets. The power and ground nets are VDD and VSS in this example.
+# The below commands will instruct the tool to connect all the pins named VSS to the ground net named VSS, 
+# all the pins named VDD to connect to the power net VDD, and all the tielow/high to respectively connect to VSS/VDD
+foreach pgnet [get_flow_config init_power_nets] {
+    puts "\033\[1;33m\[INFO\] - \033\[1;32mConnecting global net \033\[1;31m$pgnet\033\[0m"
+    connect_global_net $pgnet -type pg_pin -pin_base_name $pgnet -inst_base_name *
+    #connect_global_net $pgnet -type tie_hi -inst_base_name * -hinst {}
+}
+foreach pgnet [get_flow_config init_ground_nets] {
+    puts "\033\[1;33m\[INFO\] - \033\[1;32mConnecting global net \033\[1;31m$pgnet\033\[0m"
+    connect_global_net $pgnet -type pg_pin -pin_base_name $pgnet -inst_base_name *
+    #connect_global_net $pgnet -type tie_lo -inst_base_name * -hinst {}
+}
+
+# Connect tie-low and tie-high
+connect_global_net VDD -type tie_hi -inst_base_name * -hinst {}
+connect_global_net VSS -type tie_lo -inst_base_name * -hinst {}
+
+set pgnets [get_flow_config user_global_nets]
+foreach item [dict keys $pgnets] {
+    set value [dict get $pgnets $item]
+    puts "\033\[1;33m\[INFO\] - \033\[1;32mConnecting global net \033\[1;31m$value ($item)\033\[0m"
+    connect_global_net $value -pin_base_name $item -type pg_pin -inst_base_name * -hinst {}
+}
+
+
+
+#########################3
+# add well taps only for hvt
+if { "hvt" in [get_flow_config vths] } {
+    add_well_taps -cell [get_flow_config well_tap_cell] \
+        -cell_interval [get_flow_config welltap_interval] \
+        -prefix "WELLTAP" \
+        -checker_board
+}
+
+
+
+############################################
+# POWER GRID 
+###########################################
+
+####################################################################
+# Follow pins for VDD/VSS
+####################################################################
+set_db route_special_via_connect_to_shape { noshape }
+route_special -connect {core_pin} \
+    -layer_change_range { M1(1) M1(1) } \
+    -block_pin_target {nearest_target} \
+    -pad_pin_port_connect {all_port one_geom} \
+    -pad_pin_target {nearest_target} \
+    -core_pin_target {first_after_row_end} \
+    -floating_stripe_target {block_ring pad_ring ring stripe ring_pin block_pin followpin} \
+    -allow_jogging 0 \
+    -crossover_via_layer_range { M1(1) M1(1) } \
+    -nets { VDD VSS } \
+    -allow_layer_change 0 \
+    -block_pin use_lef \
+    -target_via_layer_range { M1(1) M1(1) } 
+
+
+# ADD STRIPES MANUALLY
+
+
+# SAVE FLOORPLAN
+set floorplan_file [string map {".latest" "" } [get_flow_config init_floorplan_file]]
+set new_floorplan_file [lindex [next_file_version_number $floorplan_file] 1]
+pinfo "Now saving floorplan to file [+ green]$new_floorplan_file[+ blue] and linking it to [+ red]$floorplan_file.latest[+]"
+write_floorplan $new_floorplan_file
+# Link to latest
+ln -sf $new_floorplan_file $floorplan_file.latest
+puts [sep]
